66960b08c302884912840602de44cacd3389a4ea
quocdung1999 <1712369@student.hcmus.edu.vn>
2020-12-07 00:17:13 +0700

    Add models

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+require("dotenv").config({
+  path: './.env',
+});
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const mongoose = require('./models/connect.js');
+const cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+const PORT = process.env.PORT || 5000;
 // view engine setup
+
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.use(cors);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -38,4 +46,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(PORT,() => console.log(`This server has started on port ${PORT}`))
+//module.exports = app;
