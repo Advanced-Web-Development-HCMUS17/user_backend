b867890fedecc3fd1582e7bd65591c9d1c54f9da
nvhieu4899 <elpulga246@gmail.com>
2020-12-08 21:40:21 +0700

    ! socket save online users

--- socket/eventConstant.js
+++ socket/eventConstant.js
@@ -2,4 +2,5 @@ const USER_EVENT = {
   ONLINE: 'user/online',
   OFFLINE: 'user/offline',
 }
-module.exports = USER_EVENT;
+const LIST_ONLINE_USER_EVENT = 'onlineUserList';
+module.exports = {USER_EVENT, LIST_ONLINE_USER_EVENT};
--- socket/index.js
+++ socket/index.js
@@ -1,5 +1,5 @@
 const Socket = require("socket.io");
-const USER_EVENT = require("./eventConstant");
+const {USER_EVENT, LIST_ONLINE_USER_EVENT} = require("./eventConstant");
 
 const cors = require('cors');
 const http = require('http');
@@ -11,15 +11,25 @@ const io = new Socket.Server(3030, {
 
 const USER_STATUS_TOPIC = 'userStatus';
 
+let users = {};
 
 io.use((socket, next) => {
+  console.log(socket.handshake.auth);
+  const auth = socket.handshake.auth;
   next();
 });
 
+
 io.on('connection', (socket) => {
 
+  users[socket.id] = socket.id;
   io.emit(USER_EVENT.ONLINE, {user: socket.id, id: socket.id});
-  socket.on('disconnect', () =>
-    io.emit(USER_EVENT.OFFLINE, {user: socket.id, id: socket.id}));
+
+  socket.emit(LIST_ONLINE_USER_EVENT, users);
+  socket.on('disconnect', () => {
+      users[socket.id] = undefined;
+      io.emit(USER_EVENT.OFFLINE, {user: socket.id, id: socket.id});
+    }
+  );
 });
 
