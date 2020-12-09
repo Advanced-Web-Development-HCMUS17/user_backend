b3ed4481b67a4086eed33b425e9c83a548afc28a
Huynh Huu Duc <1712354@student.hcmus.edu.vn>
2020-12-09 11:08:10 +0700

    !app, - routes\index, -routes\user
    Fix port conflict in app.js and bin\www to host on Heroku
    Clean some unused code.

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ require("dotenv").config({
   path: './.env',
 });
 
-const createError = require('http-errors');
+// const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('./models/connect.js');
 const cors = require('cors');
-var indexRouter = require('./routes/index');
 require('./socket');
 var app = express();
-const PORT = process.env.PORT || 5000;
+
 
 const usersRouter = require('./routes/user-route');
 
@@ -29,21 +28,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
-app.listen(PORT,() => console.log(`This server has started on port ${PORT}`));
 module.exports = app;
