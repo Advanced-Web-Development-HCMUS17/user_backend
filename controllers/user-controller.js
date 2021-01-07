8a2b07976597b26938205f4ffab9c455db5f514b
Huynh Huu Duc <1712354@student.hcmus.edu.vn>
2021-01-08 02:38:26 +0700

    +Active, reset password by email

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,41 +1,94 @@
 require('dotenv').config();
-const userModel = require('../models/userModel');
+const {User} = require('../models/userModel');
+const userVerifyModel = require('../models/userVerifyModel');
 const tokenService = require('../services/token-service');
+const mailService = require('../services/mail-service');
+
+const CLIENT_URL = process.env.CLIENT_URL;
+const SERVER_URL = process.env.SERVER_URL;
 
 exports.register = async (req, res) => {
   let {username, email, password} = req.body;
   if (!username) username = email;
 
-  if (await userModel.findOne({email: email})) {
-    res.status(400).json({message: "Email is already taken."});
-    return;
-  }
-  const newUser = new userModel({username: username, email: email, password: password});
+  if (await User.findOne({email: email}))
+    return res.status(400).send("Email is already taken.");
+
+  const newUser = new User({username: username, email: email, password: password});
   let savedUser = await newUser.save();
-  savedUser.password = undefined;
-  if (savedUser._id) {
-    res.status(201).json({message: "Success"});
-  } else {
-    res.status(500).json({message: "Error when saving user."});
-  }
+  if (!savedUser)
+    return res.status(500).send("Error occur when saving user.");
+
+  const secretCode = tokenService.sign(savedUser._id);
+  const verifyData = new userVerifyModel({ID: savedUser._id, secretCode: secretCode});
+  await verifyData.save();
+
+  const mailContent = `<p>Please use the following link within the next 15 minutes to activate your account: <strong><a href="${SERVER_URL}/users/verification/verify-account/${savedUser._id}/${secretCode}" target="_blank">Link</a></strong></p>`;
+  await mailService.send(savedUser.email, "Verify your email", mailContent);
+  res.redirect(CLIENT_URL + "/login", 201);
+}
+
+exports.verify = async (req, res) => {
+  const {userId, secretCode} = req.params;
+
+  if (!userId || !secretCode)
+    return res.status(400).send("Bad request");
+
+  const isValid = await userVerifyModel.findOne({ID: userId, secretCode: secretCode});
+  if (!isValid)
+    return res.send(404).send("Not found");
+
+  await User.findByIdAndUpdate(userId, {"isVerified": true});
+  await userVerifyModel.findOneAndDelete({ID: userId, secretCode: secretCode});
+  res.redirect(CLIENT_URL + "/login");
 }
 
 exports.login = async (req, res) => {
-  if (req.isAuthenticated()) {
-    console.log(req.user);
-    req.user.password = undefined;
-    const token = tokenService.sign(req.user);
-    res.status(200).send({token: token, userInfo: req.user});
-  } else {
-    res.status(500).send("Invalid user.");
-  }
+  if (!req.user.isVerified)
+    return res.status(403).send("Account has not been verified");
+  let user = req.user;
+  user.password = undefined;
+  res.status(200).send({token: tokenService.sign(user), userInfo: user});
 }
 
 exports.loginUsingOAuth2 = async (req, res) => {
-  if (req.user) {
-    req.user.password = undefined;
-    res.redirect(`${process.env.CLIENT_URL}/login?token=${tokenService.sign(req.user)}`);
-  } else {
-    res.redirect(`${process.env.CLIENT_URL}/login`);
-  }
+  if (!req.user)
+    return res.redirect(CLIENT_URL + '/login');
+  req.user.password = undefined;
+  res.redirect(`${CLIENT_URL}/login/${tokenService.sign(req.user)}`);
+}
+
+exports.resetPassword = async (req, res) => {
+  const {email} = req.body;
+  console.log(email);
+  if (!email)
+    return res.status(400).send("Bad request");
+
+  const user = await User.findOne({email: email});
+  if (!user)
+    return res.status(404).send("Account not exist");
+
+  console.log(user);
+  const secretCode = tokenService.sign(user.email);
+  const verifyData = new userVerifyModel({ID: user.email, secretCode: secretCode});
+  await verifyData.save();
+
+  const mailContent = `<p>Please use the following link within the next 15 minutes to reset password: <strong><a href="${CLIENT_URL}/reset-password/${user.email}/${secretCode}" target="_blank">Link</a></strong></p>`;
+  await mailService.send(user.email, "Reset password request", mailContent);
+  res.status(200).send("Success");
+}
+
+exports.updatePassword = async (req, res) => {
+  const {email, secretCode, newPassword} = req.body;
+
+  if (!email || !secretCode || !newPassword)
+    return res.status(400).send("Bad request");
+
+  const isValid = await userVerifyModel.findOne({ID: email, secretCode: secretCode});
+  if (!isValid)
+    return res.status(404).send("Reset password request was expired");
+
+  await User.findOneAndUpdate({email: email}, {password: newPassword});
+  await userVerifyModel.findOneAndDelete({ID: email, secretCode: secretCode});
+  res.status(200).send("Success");
 }
